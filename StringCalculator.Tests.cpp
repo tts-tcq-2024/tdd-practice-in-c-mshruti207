92972974797f0d185ff7ede98e2abff7495e985a
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-19 09:45:41 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,19 +1,19 @@
 #include <gtest/gtest.h>
 #include "StringCalculator.h"
 
-/*TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
+TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
     int expectedresult = 0;
     const char* input = "Hello, world!";
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
 
-TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
+/*TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
     int expectedresult = 0;
     const char* input = "0";
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
-}*/
+}
 
 TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
     int expectedresult = 3;
