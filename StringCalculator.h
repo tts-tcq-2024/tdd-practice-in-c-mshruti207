293d613a3543a7bd064d3456dbc9252a22aee0ea
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-19 09:47:43 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+#define MAX_NUMBER 1000
 
+
+int add(const char* input) {
+    if (input == NULL || strlen(input) == 0) {
+        return 0;
+    }
+
+}
